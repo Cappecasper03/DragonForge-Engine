27dc257d59a6eb36b3306c55b71ae89ddd5d4033
Casper Juvas <caspjuva@playgroundsquad.com>
2023-10-11 16:31:12 +0200

    Heap memory tracker using overloaded new and delete

---
+++ source/core/memory/Memory.cpp
@@ -0,0 +1,67 @@
+#include "Memory.h"
+
+#include <cstdlib>
+#include <map>
+#include <vector>
+#include <string>
+
+namespace Memory
+{
+    struct sMemory
+    {
+        void*       address  = nullptr;
+        size_t      size     = 0;
+        const char* file     = nullptr;
+        int         line     = 0;
+        const char* function = nullptr;
+    };
+
+    std::map< size_t, sMemory > memories;
+
+    size_t usage     = 0;
+    size_t max_usage = 0;
+
+    float getUsageKb() { return static_cast< float >( usage ) / 1000; }
+    float getMaxUsageKb() { return static_cast< float >( max_usage ); }
+};
+
+void* operator new( const size_t _size, const char* _file, const int _line, const char* _function )
+{
+    void* address = malloc( _size );
+    Memory::usage += _size;
+
+    if( Memory::usage > Memory::max_usage )
+        Memory::max_usage = Memory::usage;
+
+    Memory::sMemory memory;
+    memory.address  = address;
+    memory.size     = _size;
+    memory.file     = _file;
+    memory.line     = _line;
+    memory.function = _function;
+
+    constexpr std::hash< std::string > string_hash;
+    constexpr std::hash< int >         int_hash;
+    const size_t                       file_hash( string_hash( std::string( _file ) ) );
+    const size_t                       function_hash( string_hash( std::string( _function ) ) );
+    const size_t                       line_hash( int_hash( _line ) );
+
+    const size_t hash        = file_hash + function_hash * line_hash;
+    Memory::memories[ hash ] = memory;
+
+    return address;
+}
+
+void operator delete( void* _address, const char* _file, const int _line, const char* _function )
+{
+    constexpr std::hash< std::string > string_hash;
+    constexpr std::hash< int >         int_hash;
+    const size_t                       file_hash( string_hash( std::string( _file ) ) );
+    const size_t                       function_hash( string_hash( std::string( _function ) ) );
+    const size_t                       line_hash( int_hash( _line ) );
+    const size_t                       hash = file_hash + function_hash * line_hash;
+
+    Memory::usage -= Memory::memories[ hash ].size;
+    Memory::memories.erase( hash );
+    free( _address );
+}
---
+++ source/core/memory/Memory.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+namespace Memory
+{
+    extern float getUsageKb();
+    extern float getMaxUsageKb();
+}
+
+#if defined( DEBUG )
+#undef new
+
+extern void* operator new( const size_t _size, const char* _file, const int _line, const char* _function );
+extern void  operator delete( void* _address, const char* _file, const int _line, const char* _function );
+
+#define NEW new ( __FILE__, __LINE__, __FUNCTION__ )
+#define DELETE delete ( __FILE__, __LINE__, __FUNCTION__ )
+
+#else
+
+#define NEW new;
+#define DELETE delete;
+
+#endif
