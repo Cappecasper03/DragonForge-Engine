10ce1a3358285b1984dc6d2e849078f53626bb2c
Casper Juvas <caspjuva@playgroundsquad.com>
2023-10-12 14:52:35 +0200

    Basic start on profiling

--- source/core/profiling/Profiling.cpp
+++ source/core/profiling/Profiling.cpp
@@ -1,1 +1,37 @@
 ﻿#include "Profiling.h"
+
+#include <stack>
+#include <vector>
+
+namespace Profiling
+{
+    struct sProfiling
+    {
+        std::string message       = {};
+        double      time_begin    = 0;
+        double      time_total    = 0;
+        double      time_external = 0;
+        double      time_min      = 0;
+        double      time_max      = 0;
+        uint32_t    call_count    = 0;
+    };
+
+    unsigned index_counter = 0;
+
+    std::vector< sProfiling* > profiling_vector;
+    std::stack< sProfiling >  profiling_stack;
+
+    void begin( const unsigned _index, const std::string& _message )
+    {
+        _index;
+        _message;
+    }
+
+    void end()
+    {}
+
+    unsigned generateIndex()
+    {
+        return index_counter++;
+    }
+}
--- source/core/profiling/Profiling.h
+++ source/core/profiling/Profiling.h
@@ -1,4 +1,32 @@
 ﻿#pragma once
 
+#include <string>
+
 namespace Profiling
-{};
+{
+    extern void     begin( const unsigned _index, const std::string& _message );
+    extern void     end();
+    extern unsigned generateIndex();
+
+    struct sProfilingScope
+    {
+        sProfilingScope( const sProfilingScope& ) = delete;
+        sProfilingScope( sProfilingScope&& )      = delete;
+        void operator=( const sProfilingScope& )  = delete;
+        void operator=( sProfilingScope&& )       = delete;
+
+        sProfilingScope( const unsigned _index, const std::string& _message ) { begin( _index, _message ); }
+        ~sProfilingScope() { end(); }
+    };
+};
+
+#define PROFILING_UNIQUE( _name, _line ) _name ## _line
+#define PROFILING_UNIQUE_INDEX( _line ) PROFILING_UNIQUE( _PROFILING_INDEX, _line )
+#define PROFILING_UNIQUE_SCOPE( _line ) PROFILING_UNIQUE( _PROFILING_SCOPE, _line )
+
+#define PROFILING_UNIQUE_INDEX_NAME PROFILING_UNIQUE_INDEX( __LINE__ )
+#define PROFILING_UNIQUE_SCOPE_NAME PROFILING_UNIQUE_SCOPE( __LINE__ )
+
+#define PROFILING_SCOPE( _message ) static const unsigned PROFILING_UNIQUE_INDEX_NAME = Profiling::generateIndex(); const Profiling::sProfilingScope PROFILING_UNIQUE_SCOPE_NAME( PROFILING_UNIQUE_INDEX_NAME, _message );
+#define PROFILING_BEGIN( _message ) static const unsigned PROFILING_UNIQUE_INDEX_NAME = Profiling::generateIndex(); Profiling::begin( PROFILING_UNIQUE_INDEX_NAME, _message );
+#define PROFILING_END Profiling::end()
