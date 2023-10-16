2ac25711bd7ac71c90c4b92777b5928d9a903761
Cappecasper03 <03repsac@gmail.com>
2023-10-16 22:42:49 +0200

    Created 2d vector class

---
+++ source/core/math/cVector2.h
@@ -0,0 +1,176 @@
+﻿#pragma once
+
+#include <cmath>
+
+namespace vg
+{
+    template< typename T >
+    class cVector2
+    {
+    public:
+        typedef cVector2< float > cVector2f;
+
+        cVector2()
+        : x( 0 ),
+          y( 0 )
+        {}
+
+        explicit cVector2( const T& _value )
+        : x( _value ),
+          y( _value )
+        {}
+
+        cVector2( const T& _x, const T& _y, const T& _z )
+        : x( _x ),
+          y( _y )
+        {}
+
+        cVector2( const cVector2& _other )
+        : x( _other.x ),
+          y( _other.y )
+        { }
+
+        cVector2( cVector2&& _other ) = default;
+        ~cVector2()                   = default;
+
+        cVector2& operator=( const T& _value )
+        {
+            x = _value;
+            y = _value;
+            return *this;
+        }
+
+        cVector2& operator=( const T& _x, const T& _y, const T& _z )
+        {
+            x = _x;
+            y = _y;
+            return *this;
+        }
+
+        cVector2& operator=( const cVector2& _other )
+        {
+            if( this != &_other )
+            {
+                x = _other.x;
+                y = _other.y;
+            }
+            return *this;
+        }
+
+        cVector2& operator=( cVector2&& _other ) = default;
+
+        cVector2 operator+( const T& _value ) { return cVector2( x + _value, y + _value ); }
+        cVector2 operator+( const cVector2& _other ) { return cVector2( x + _other.x, y + _other.y ); }
+
+        cVector2& operator+=( const T& _value )
+        {
+            x += _value;
+            y += _value;
+            return *this;
+        }
+
+        cVector2& operator+=( const cVector2& _other )
+        {
+            if( this != &_other )
+            {
+                x += _other.x;
+                y += _other.y;
+            }
+            return *this;
+        }
+
+        cVector2 operator-( const T& _value ) { return cVector2( x - _value, y - _value ); }
+        cVector2 operator-( const cVector2& _other ) { return cVector2( x - _other.x, y - _other.y ); }
+
+        cVector2& operator-=( const T& _value )
+        {
+            x -= _value;
+            y -= _value;
+            return *this;
+        }
+
+        cVector2& operator-=( const cVector2& _other )
+        {
+            if( this != &_other )
+            {
+                x -= _other.x;
+                y -= _other.y;
+            }
+            return *this;
+        }
+
+        cVector2 operator*( const T& _value ) { return cVector2( x * _value, y * _value ); }
+        cVector2 operator*( const cVector2& _other ) { return cVector2( x * _other.x, y * _other.y ); }
+
+        cVector2& operator*=( const T& _value )
+        {
+            x *= _value;
+            y *= _value;
+            return *this;
+        }
+
+        cVector2& operator*=( const cVector2& _other )
+        {
+            if( this != &_other )
+            {
+                x *= _other.x;
+                y *= _other.y;
+            }
+            return *this;
+        }
+
+        cVector2 operator/( const T& _value ) { return cVector2( x / _value, y / _value ); }
+        cVector2 operator/( const cVector2& _other ) { return cVector2( x / _other.x, y / _other.y ); }
+
+        cVector2& operator/=( const T& _value )
+        {
+            x /= _value;
+            y /= _value;
+            return *this;
+        }
+
+        cVector2& operator/=( const cVector2& _other )
+        {
+            if( this != &_other )
+            {
+                x /= _other.x;
+                y /= _other.y;
+            }
+            return *this;
+        }
+
+        double length() { return sqrt( pow( x, 2 ) + pow( y, 2 ) ); }
+        double squaredLength() { return pow( x, 2 ) + pow( y, 2 ); }
+
+        cVector2& normalize()
+        {
+            double len = length();
+            x /= len;
+            y /= len;
+            return *this;
+        }
+
+        cVector2 normalized()
+        {
+            double len = length();
+            if( len != 0 )
+                return cVector2( x / len, y / len );
+
+            return *this;
+        }
+
+        double dot( const cVector2& _other ) { return x * _other.x + y * _other.y; }
+        double dot( const cVector2* _other ) { return x * _other->x + y * _other->y; }
+
+        double angleBetween( const cVector2& _other ) { return acos( dot( _other ) / ( length() * _other.length() ) ); }
+        double angleBetween( const cVector2* _other ) { return acos( dot( _other ) / ( length() * _other->length() ) ); }
+        double angleBetweenNormalized( const cVector2& _other ) { return acos( dot( _other ) ); }
+        double angleBetweenNormalized( const cVector2* _other ) { return acos( dot( _other ) ); }
+
+        cVector2 project( const cVector2& _other ) { return _other * ( dot( _other ) / _other.dot( this ) ); }
+        cVector2 project( const cVector2* _other ) { return _other * ( dot( _other ) / _other->dot( this ) ); }
+
+        T x;
+        T y;
+    };
+}
