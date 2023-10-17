250f4e98334d6bc298f22c65d677487dd72d1197
Cappecasper03 <03repsac@gmail.com>
2023-10-18 00:05:27 +0200

    Transform class

---
+++ source/core/math/cTransform.cpp
@@ -0,0 +1,91 @@
+﻿#include "cTransform.h"
+
+#include <algorithm>
+
+#include "core/log/Log.h"
+
+namespace df
+{
+    cTransform::cTransform()
+    : m_parent( nullptr )
+    {}
+
+    cTransform::~cTransform()
+    {
+        if( m_parent )
+            removeParent();
+
+        while( !m_children.empty() )
+            removeChild( *m_children.front() );
+    }
+
+    void cTransform::update()
+    {
+        m_render_matrix = m_parent ? matrix * m_parent->m_render_matrix : matrix;
+
+        for( cTransform* child : m_children )
+            child->update();
+    }
+
+    bool cTransform::addChild( cTransform& _child )
+    {
+        if( this == &_child )
+        {
+            LOG_ERROR( "Child can't be itself" );
+            return false;
+        }
+
+        if( _child.m_parent )
+        {
+            LOG_ERROR( "Child already have a parent" );
+            return false;
+        }
+
+        m_children.push_back( &_child );
+        _child.m_parent = this;
+        return true;
+    }
+
+    bool cTransform::removeChild( cTransform& _child )
+    {
+        if( std::erase( m_children, &_child ) )
+        {
+            _child.m_parent = nullptr;
+            return true;
+        }
+
+        LOG_WARNING( "Child doesn't exist" );
+        return false;
+    }
+
+    bool cTransform::removeParent()
+    {
+        if( !m_parent )
+        {
+            LOG_WARNING( "Parent doesn't exist" );
+            return false;
+        }
+
+        m_parent->removeChild( *this );
+        return true;
+    }
+
+    bool cTransform::setParent( cTransform& _parent )
+    {
+        if( this == &_parent )
+        {
+            LOG_WARNING( "Parent can't be itself" );
+            return false;
+        }
+
+        if( this->m_parent )
+        {
+            LOG_WARNING( "Already have a parent" );
+            return false;
+        }
+
+        m_parent = &_parent;
+        _parent.m_children.push_back( this );
+        return true;
+    }
+}
---
+++ source/core/math/cTransform.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include <vector>
+
+#include "cMatrix4x4.h"
+
+namespace df
+{
+    class cTransform final
+    {
+    public:
+        cTransform();
+        cTransform( const cTransform& _other ) = delete;
+        cTransform( cTransform&& _other )      = default;
+        ~cTransform();
+
+        cTransform& operator=( const cTransform& )   = delete;
+        cTransform& operator=( cTransform&& _other ) = default;
+
+        void update();
+
+        bool addChild( cTransform& _child );
+        bool removeChild( cTransform& _child );
+
+        bool setParent( cTransform& _parent );
+        bool removeParent();
+
+        const cMatrix4X4f& getWorldMatrix() { return m_render_matrix; }
+
+        cMatrix4X4f matrix;
+
+    private:
+        cMatrix4X4f                m_render_matrix;
+        cTransform*                m_parent;
+        std::vector< cTransform* > m_children;
+    };
+}
