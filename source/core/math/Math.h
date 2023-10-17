873512a444b99a4c7037124b5b706ce73e43683f
Cappecasper03 <03repsac@gmail.com>
2023-10-17 19:50:23 +0200

    Created interpolation function in math namespace

--- source/core/math/Math.h
+++ source/core/math/Math.h
@@ -9,4 +9,7 @@ namespace df::math
 
     template< typename T >
     T degToRad( const T& _value ) { return _value * ( std::numbers::pi_v< T > / T( 180 ) ); }
+
+    template< typename T >
+    T interpolate( const T& _current, const T& _target, const float& _delta_time, const float& _speed ) { return _current + ( _target - _current ) * _delta_time * _speed; }
 }
