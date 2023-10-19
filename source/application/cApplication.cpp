6eac871f7f2d01edff739c5c2322262ed485404f
Cappecasper03 <03repsac@gmail.com>
2023-10-19 23:27:00 +0200

    Enable depth test

--- source/application/cApplication.cpp
+++ source/application/cApplication.cpp
@@ -191,13 +191,15 @@ void cApplication::run()
 
     df::cTimer timer;
 
+    glEnable( GL_DEPTH_TEST );
+
     while( !glfwWindowShouldClose( m_window ) )
     {
         input();
         update();
 
         glClearColor( 0.2f, 0.3f, 0.3f, 1.0f );
-        glClear( GL_COLOR_BUFFER_BIT );
+        glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 
         glBindTexture( GL_TEXTURE_2D, texture );
 
