a3ba447b709e519e3f04d9c121a7ed7fd13845f1
Casper Juvas <caspjuva@playgroundsquad.com>
2023-10-19 14:43:28 +0200

    Created cEventManager ( Needs Testing )

---
+++ source/core/managers/cEventManager.cpp
@@ -0,0 +1,52 @@
+#include "cEventManager.h"
+
+#include <ranges>
+
+namespace df
+{
+    cEventManager::~cEventManager()
+    {
+        for( sEvent* event : m_events | std::views::values )
+        {
+            if( event )
+                MEMORY_FREE( event );
+        }
+    }
+
+    void cEventManager::subscribe( const std::string& _name, void* _object, std::function< void( void* ) > _function )
+    {
+        sEvent* event = getInstance()->m_events[ _name ];
+
+        if( !event )
+        {
+            event       = MEMORY_ALLOC( sEvent, 1 );
+            event->name = _name;
+        }
+
+        event->subscribers[ _object ] = _function;
+    }
+
+    void cEventManager::unsubscribe( const std::string& _name, void* _object )
+    {
+        sEvent* event = getInstance()->m_events[ _name ];
+
+        if( !event )
+            return;
+
+        event->subscribers.erase( _object );
+
+        if( event->subscribers.empty() )
+            getInstance()->m_events.erase( _name );
+    }
+
+    void cEventManager::invoke( const std::string& _name, void* _data )
+    {
+        sEvent* event = getInstance()->m_events[ _name ];
+
+        if( !event )
+            return;
+
+        for( std::function< void( void* ) >& function : event->subscribers | std::views::values )
+            function( _data );
+    }
+}
---
+++ source/core/managers/cEventManager.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+
+#include <functional>
+#include <string>
+#include <unordered_map>
+
+#include "core/misc/iSingleton.h"
+
+namespace df
+{
+    namespace event
+    {
+        static std::string input     = "input";
+        static std::string update    = "update";
+        static std::string render_3d = "render3D";
+        static std::string render_2d = "render2D";
+    }
+
+    class cEventManager final : public iSingleton< cEventManager >
+    {
+    public:
+        DISABLE_COPY_AND_MOVE( cEventManager );
+
+        cEventManager() = default;
+        ~cEventManager() override;
+
+        static void subscribe( const std::string& _name, void* _object, std::function< void( void* ) > _function );
+        static void unsubscribe( const std::string& _name, void* _object );
+
+        static void invoke( const std::string& _name, void* _data );
+
+    private:
+        struct sEvent
+        {
+            std::string                                                       name;
+            std::unordered_map< const void*, std::function< void( void* ) > > subscribers;
+        };
+
+        std::unordered_map< std::string, sEvent* > m_events;
+    };
+}
