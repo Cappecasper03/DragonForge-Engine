81b70bff9e98bbbaa20956fe73e3bb573141b033
Cappecasper03 <03repsac@gmail.com>
2023-10-19 18:04:46 +0200

    Made cEventManager use named events and being able to unsubscribe from them

--- source/core/managers/cEventManager.h
+++ source/core/managers/cEventManager.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 
 #include <functional>
+#include <ranges>
 #include <string>
 #include <unordered_map>
 
@@ -26,25 +27,37 @@ namespace df
 
         template< typename T, typename... Targs >
         static void subscribe( const std::string& _name, T* _object, void ( T::*_function )( Targs... ) );
-        static void unsubscribe( const std::string& _name, void* _object );
+
+        template< typename T >
+        static void unsubscribe( const std::string& _name, T* _object );
 
         template< typename... Targs >
         static void invoke( const std::string& _name, Targs... _args );
 
     private:
-        std::vector< std::function< void() > > m_events;
+        typedef std::unordered_map< void*, std::function< void() > > event;
+
+        std::unordered_map< std::string, event > m_events;
     };
 
     template< typename T, typename... Targs >
-    void cEventManager::subscribe( const std::string& /*_name*/, T* _object, void ( T::*_function )( Targs... ) )
+    void cEventManager::subscribe( const std::string& _name, T* _object, void ( T::*_function )( Targs... ) )
     {
-        getInstance()->m_events.push_back( [_object, _function]( Targs... _args ) { ( _object->*_function )( _args... ); } );
+        event& event     = getInstance()->m_events[ _name ];
+        event[ _object ] = [_object, _function]( Targs... _args ) { ( _object->*_function )( _args... ); };
+    }
+
+    template< typename T >
+    void cEventManager::unsubscribe( const std::string& _name, T* _object )
+    {
+        event& event = getInstance()->m_events[ _name ];
+        event.erase( _object );
     }
 
     template< typename... Targs >
-    void cEventManager::invoke( const std::string& /*_name*/, Targs... _args )
+    void cEventManager::invoke( const std::string& _name, Targs... _args )
     {
-        for( std::function< void() >& event : getInstance()->m_events )
-            event( _args... );
+        for( std::function< void() >& function : getInstance()->m_events[ _name ] | std::ranges::views::values )
+            function( _args... );
     }
 }
