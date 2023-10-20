e0151e380dec5ee6fbcba60b67347c9522a15dfb
Cappecasper03 <03repsac@gmail.com>
2023-10-21 00:00:03 +0200

    Created the basics of a free flight camera

---
+++ source/core/rendering/camera/cFreeFlightCamera.cpp
@@ -0,0 +1,18 @@
+﻿#include "cFreeFlightCamera.h"
+
+namespace df
+{
+    cFreeFlightCamera::cFreeFlightCamera( const float& _speed )
+    : m_speed( _speed ),
+      m_rotation( .0f ),
+      m_position( .0f )
+    {}
+
+    void cFreeFlightCamera::update( const float& /*_delta_time*/ )
+    {
+        cCamera::update();
+    }
+
+    void cFreeFlightCamera::input( const input::sInput& /*_input*/ )
+    { }
+}
