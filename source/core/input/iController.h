4d2232e6d42dd1f98de7dea9908247eb7d0062ab
Cappecasper03 <03repsac@gmail.com>
2023-10-20 22:19:28 +0200

    Created a simple iController class

---
+++ source/core/input/iController.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "Input.h"
+
+namespace df
+{
+    class iController
+    {
+    public:
+        iController()          = default;
+        virtual ~iController() = default;
+
+        void setActive( const bool& _active ) { m_active = _active; }
+
+        virtual void input( const input::sInput& _input ) = 0;
+
+    protected:
+        bool m_active;
+    };
+}
