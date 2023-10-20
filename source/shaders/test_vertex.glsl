ab37706117b793178e2ec3edc86fa415c4ef65a4
Cappecasper03 <03repsac@gmail.com>
2023-10-20 18:24:43 +0200

    Moved variables in shader

--- source/shaders/test_vertex.glsl
+++ source/shaders/test_vertex.glsl
@@ -1,11 +1,11 @@
 #version 460 core
 
-layout ( location = 0 ) in vec3 i_position;
-layout ( location = 1 ) in vec2 i_tex_coord;
-
 uniform mat4 u_model;
 uniform mat4 u_view;
 uniform mat4 u_projection;
+
+layout ( location = 0 ) in vec3 i_position;
+layout ( location = 1 ) in vec2 i_tex_coord;
 
 out vec2 tex_coord;
 
