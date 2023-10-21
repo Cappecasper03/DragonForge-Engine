8c016e76890a11155cde14438c7aca1c03b9dff6
Cappecasper03 <03repsac@gmail.com>
2023-10-21 20:43:04 +0200

    Simple iRenderAsset class

---
+++ source/core/rendering/assets/iRenderAsset.cpp
@@ -0,0 +1,21 @@
+﻿#include "iRenderAsset.h"
+
+#include <glad/glad.h>
+
+namespace df
+{
+    iRenderAsset::iRenderAsset()
+    : matrix( 1 )
+    {
+        glGenVertexArrays( 1, &m_vao );
+        glGenBuffers( 1, &m_vbo );
+        glGenBuffers( 1, &m_ebo );
+    }
+
+    iRenderAsset::~iRenderAsset()
+    {
+        glDeleteBuffers( 1, &m_ebo );
+        glDeleteBuffers( 1, &m_vbo );
+        glDeleteVertexArrays( 1, &m_vao );
+    }
+}
---
+++ source/core/rendering/assets/iRenderAsset.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include <glm/ext/matrix_float4x4.hpp>
+
+#include "core/misc/Misc.h"
+
+namespace df
+{
+    class iRenderAsset
+    {
+    public:
+        DISABLE_COPY_AND_MOVE( iRenderAsset );
+
+        iRenderAsset();
+        virtual ~iRenderAsset();
+
+        virtual void render() = 0;
+
+        glm::mat4 matrix;
+
+    protected:
+        unsigned m_vao;
+        unsigned m_vbo;
+        unsigned m_ebo;
+    };
+}
