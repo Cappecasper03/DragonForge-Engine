95d4d5cd527ce6d8d8fca87a665a3a0662a16eb9
Cappecasper03 <03repsac@gmail.com>
2023-10-22 13:49:55 +0200

    Created cRenderCallback class

---
+++ source/core/rendering/callback/cRenderCallback.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+#include <functional>
+
+#include "core/memory/Memory.h"
+#include "core/misc/Misc.h"
+
+namespace df
+{
+    class cShader;
+
+    struct iRenderCallback
+    {
+        DISABLE_COPY_AND_MOVE( iRenderCallback );
+
+        iRenderCallback()          = default;
+        virtual ~iRenderCallback() = default;
+    };
+
+    template< typename... Targs >
+    class cRenderCallback final : public iRenderCallback
+    {
+    public:
+        DISABLE_COPY_AND_MOVE( cRenderCallback );
+
+        explicit cRenderCallback( const std::string& _shader_name, void ( _callback )( const cShader*, Targs... ) );
+        ~cRenderCallback() override;
+
+        void render( Targs... _args ) { m_callback( m_shader, _args ); }
+
+    protected:
+        cShader*                                          m_shader;
+        std::function< void( const cShader*, Targs... ) > m_callback;
+    };
+
+    template< typename... Targs >
+    cRenderCallback< Targs... >::cRenderCallback( const std::string& _shader_name, void ( _callback )( const cShader*, Targs... ) )
+    {
+        m_shader   = MEMORY_ALLOC( cShader, 1, _shader_name );
+        m_callback = _callback;
+    }
+
+    template< typename... Targs >
+    cRenderCallback< Targs... >::~cRenderCallback()
+    {
+        MEMORY_FREE( m_shader );
+    }
+}
