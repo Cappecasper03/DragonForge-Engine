7588a0c5d877c742f69fdab6c0110ec2312e7ef9
Cappecasper03 <03repsac@gmail.com>
2023-10-24 20:22:59 +0200

    updated libraries

--- utils/premake5/libraries.lua
+++ utils/premake5/libraries.lua
@@ -5,7 +5,7 @@ filter {}
     links { "opengl32.lib" }
 
 filter "configurations:Debug"
-    links { "assimp-vc143-mt-d.lib", "glfw3-d.lib" }
+    links { "assimp-vc143-mtd.lib", "glfw3-d.lib" }
 
 filter "configurations:Release"
     links { "assimp-vc143-mt.lib", "glfw3.lib" }
