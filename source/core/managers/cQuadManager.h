6df436c2d9e584d1d862bfe0e9e4bd659f1c6c5b
Casper Juvas <caspjuva@playgroundsquad.com>
2023-10-24 12:03:51 +0200

    Created cQuadManager and it creates the default quad render callback

---
+++ source/core/managers/cQuadManager.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "iRenderAssetManager.h"
+
+namespace df
+{
+    class cQuad;
+
+    class cQuadManager final : public iRenderAssetManager< cQuadManager, cQuad >
+    {
+    public:
+        DISABLE_COPY_AND_MOVE( cQuadManager )
+
+        cQuadManager();
+        ~cQuadManager() override = default;
+    };
+}
