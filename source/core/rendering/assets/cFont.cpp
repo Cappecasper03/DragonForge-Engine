bc2582fd46eaa9637eeb93b0bceb57dfd78750b6
Casper Juvas <caspjuva@playgroundsquad.com>
2023-10-25 14:29:06 +0200

    Initialize buffer data

--- source/core/rendering/assets/cFont.cpp
+++ source/core/rendering/assets/cFont.cpp
@@ -9,7 +9,9 @@
 namespace df
 {
     cFont::cFont( const std::string& _font_file )
-    : m_bitmap( nullptr )
+    : m_bitmap( nullptr ),
+      m_vertices{},
+      m_indices{ 0, 1, 3, 1, 2, 3 }
     {
         if( !_font_file.empty() )
             load( _font_file );
@@ -40,15 +42,12 @@ namespace df
             return false;
         }
 
-        FT_Set_Pixel_Sizes( face, 0, 48 );
+        constexpr int pixel_height = 48;
+        FT_Set_Pixel_Sizes( face, 0, pixel_height );
 
         if( m_bitmap )
-        {
             MEMORY_FREE( m_bitmap );
-            m_bitmap = MEMORY_ALLOC( cTexture, 1, GL_TEXTURE_2D_ARRAY );
-        }
-        else
-            m_bitmap = MEMORY_ALLOC( cTexture, 1, GL_TEXTURE_2D_ARRAY );
+        m_bitmap = MEMORY_ALLOC( cTexture, 1, GL_TEXTURE_2D_ARRAY );
 
         m_bitmap->bind();
         m_bitmap->setPixelStoreI( GL_UNPACK_ALIGNMENT, 1 );
@@ -57,6 +56,7 @@ namespace df
         m_bitmap->setTextureParameterI( GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE );
         m_bitmap->setTextureParameterI( GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE );
 
+        int width_max = 0;
         for( unsigned char c = 0; c < 128; ++c )
         {
             if( FT_Load_Char( face, c, FT_LOAD_RENDER ) )
@@ -70,23 +70,39 @@ namespace df
             character.bearing    = { face->glyph->bitmap_left, face->glyph->bitmap_top };
             character.advance    = face->glyph->advance.x;
 
+            width_max = character.size.x > width_max ? character.size.x : width_max;
             m_characters.insert( std::pair< char, sCharacter >( c, character ) );
 
-            glTextureSubImage3D( m_bitmap->getTexture(), 0, 0, 0, c, character.size.y, character.size.y, 1, GL_RG, GL_UNSIGNED_BYTE, face->glyph->bitmap.buffer );
+            glTextureSubImage3D( m_bitmap->getTexture(), 0, 0, 0, c, character.size.x, character.size.y, 1, GL_RED, GL_UNSIGNED_BYTE, face->glyph->bitmap.buffer );
         }
 
         m_bitmap->unbind();
         FT_Done_Face( face );
         FT_Done_FreeType( library );
+
+        m_vertices[ 0 ] = { glm::vec3( width_max, pixel_height, 0 ), glm::vec2( 1, 1 ) };
+        m_vertices[ 1 ] = { glm::vec3( width_max, 0, 0 ), glm::vec2( 1, 0 ) };
+        m_vertices[ 2 ] = { glm::vec3( 0, 0, 0 ), glm::vec2( 0, 0 ) };
+        m_vertices[ 3 ] = { glm::vec3( 0, pixel_height, 0 ), glm::vec2( 0, 1 ) };
+
+        glBindVertexArray( vertex_array_object );
+
+        glBindBuffer( GL_ARRAY_BUFFER, m_vbo );
+        glBufferData( GL_ARRAY_BUFFER, sizeof( m_vertices ), m_vertices, GL_STATIC_DRAW );
+
+        glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, m_ebo );
+        glBufferData( GL_ELEMENT_ARRAY_BUFFER, sizeof( m_indices ), m_indices, GL_STATIC_DRAW );
+
+        glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, sizeof( *m_vertices ), nullptr );
+        glEnableVertexAttribArray( 0 );
+
+        glVertexAttribPointer( 1, 2, GL_FLOAT, GL_FALSE, sizeof( *m_vertices ), reinterpret_cast< void* >( sizeof( m_vertices->position ) ) );
+        glEnableVertexAttribArray( 1 );
+
+        glBindVertexArray( 0 );
         return true;
     }
 
-    void cFont::render( const std::string& _text )
-    {
-        for( char c : _text )
-        {
-            const sCharacter& character = m_characters[ c ];
-            character;
-        }
-    }
+    void cFont::render( const glm::vec2& /*_position*/, const std::string& /*_text*/, const glm::vec2& /*_scale*/, const cColor& /*_color*/ )
+    { }
 }
--- source/core/rendering/assets/cFont.h
+++ source/core/rendering/assets/cFont.h
@@ -4,11 +4,13 @@
 #include <glm/vec2.hpp>
 
 #include "cTexture.h"
+#include "iRenderAsset.h"
+#include "core/misc/cColor.h"
 #include "core/misc/Misc.h"
 
 namespace df
 {
-    class cFont
+    class cFont : public iRenderAsset
     {
     public:
         DISABLE_COPY_AND_MOVE( cFont )
@@ -18,7 +20,7 @@ namespace df
 
         bool load( const std::string& _font_file );
 
-        void render( const std::string& _text );
+        void render( const glm::vec2& _position, const std::string& _text, const glm::vec2& _scale = glm::vec2( 1 ), const cColor& _color = color::white );
 
         cTexture* getBitmap() const { return m_bitmap; }
 
@@ -30,7 +32,16 @@ namespace df
             unsigned   advance;
         };
 
+        struct sVertex
+        {
+            glm::vec3 position;
+            glm::vec2 tex_coord;
+        };
+
         std::unordered_map< char, sCharacter > m_characters;
         cTexture*                              m_bitmap;
+
+        sVertex  m_vertices[ 4 ];
+        unsigned m_indices[ 6 ];
     };
 }
