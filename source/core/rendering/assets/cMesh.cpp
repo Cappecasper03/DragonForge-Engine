b4bc593ecdbeb35ddbf23a6a049eb1dcf12d6944
Cappecasper03 <03repsac@gmail.com>
2023-12-20 19:17:35 +0100

    Made the model.fbx work as it should

--- source/core/rendering/assets/cMesh.cpp
+++ source/core/rendering/assets/cMesh.cpp
@@ -47,24 +47,19 @@ namespace df
             if( _mesh->mVertices )
             {
                 const aiVector3D& ai_vertex = _mesh->mVertices[ i ];
-                vertex.position.x           = ai_vertex.x;
-                vertex.position.y           = ai_vertex.y;
-                vertex.position.z           = ai_vertex.z;
+                vertex.position             = { ai_vertex.x, ai_vertex.y, ai_vertex.z };
             }
 
             if( _mesh->mNormals )
             {
                 const aiVector3D& ai_normal = _mesh->mNormals[ i ];
-                vertex.normal.x             = ai_normal.x;
-                vertex.normal.y             = ai_normal.y;
-                vertex.normal.z             = ai_normal.z;
+                vertex.normal               = { ai_normal.x, ai_normal.y, ai_normal.z };
             }
 
             if( *_mesh->mTextureCoords )
             {
                 const aiVector3D* ai_texture_coords = *_mesh->mTextureCoords;
-                vertex.tex_coords.x                 = ai_texture_coords[ i ].x;
-                vertex.tex_coords.y                 = ai_texture_coords[ i ].y;
+                vertex.tex_coords                   = { ai_texture_coords->x, ai_texture_coords->x };
             }
 
             vertices.push_back( vertex );
@@ -124,18 +119,18 @@ namespace df
         glBindVertexArray( vertex_array_object );
 
         glBindBuffer( GL_ARRAY_BUFFER, m_vbo );
-        glBufferData( GL_ARRAY_BUFFER, sizeof( vertices.front() ) * vertices.size(), vertices.data(), GL_STATIC_DRAW );
+        glBufferData( GL_ARRAY_BUFFER, sizeof( sVertex ) * vertices.size(), &vertices.front(), GL_STATIC_DRAW );
 
         glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, m_ebo );
-        glBufferData( GL_ELEMENT_ARRAY_BUFFER, sizeof( indices.front() ) * indices.size(), indices.data(), GL_STATIC_DRAW );
+        glBufferData( GL_ELEMENT_ARRAY_BUFFER, sizeof( unsigned ) * indices.size(), &indices.front(), GL_STATIC_DRAW );
 
-        glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, sizeof( vertices.front() ), nullptr );
+        glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, sizeof( sVertex ), nullptr );
         glEnableVertexAttribArray( 0 );
 
-        glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, sizeof( vertices.front() ), reinterpret_cast< void* >( sizeof( vertices.front().position ) ) );
+        glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, sizeof( sVertex ), reinterpret_cast< void* >( offsetof( sVertex, sVertex::normal ) ) );
         glEnableVertexAttribArray( 1 );
 
-        glVertexAttribPointer( 2, 2, GL_FLOAT, GL_FALSE, sizeof( vertices.front() ), reinterpret_cast< void* >( sizeof( vertices.front().position ) + sizeof( vertices.front().normal ) ) );
+        glVertexAttribPointer( 2, 2, GL_FLOAT, GL_FALSE, sizeof( sVertex ), reinterpret_cast< void* >( offsetof( sVertex, sVertex::tex_coords ) ) );
         glEnableVertexAttribArray( 2 );
 
         glBindVertexArray( 0 );
