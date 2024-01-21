9fc37455418563d2f692fd45f6616b33b7ca9eb3
Cappecasper03 <03repsac@gmail.com>
2024-01-21 22:36:09 +0100

    Some space calculations for deferred shaders

--- source/shaders/default/default_mesh_deferred_fragment.glsl
+++ source/shaders/default/default_mesh_deferred_fragment.glsl
@@ -3,8 +3,8 @@
 in vert_frag
 {
     vec3 position;
-    vec3 normal;
     vec2 tex_coord;
+    mat3 tbn;
 } IN;
 
 layout ( location = 0 ) out vec3 o_position;
@@ -12,12 +12,16 @@ layout ( location = 1 ) out vec3 o_normal;
 layout ( location = 2 ) out vec4 o_color_specular;
 
 uniform sampler2D u_color_texture;
+uniform sampler2D u_normal_texture;
 uniform sampler2D u_specular_texture;
 
 void main( )
 {
+    const vec3 normal_map = texture( u_normal_texture, IN.tex_coord ).xyz * 2 - 1;
+    const vec3 normal = normalize( IN.tbn * normal_map ) - 1 / 2;
+
     o_position = IN.position;
-    o_normal = IN.normal;
+    o_normal = normal;
     o_color_specular.rgb = texture( u_color_texture, IN.tex_coord ).rgb;
     o_color_specular.a = texture( u_specular_texture, IN.tex_coord ).r;
 }
--- source/shaders/default/default_quad_deferred_fragment.glsl
+++ source/shaders/default/default_quad_deferred_fragment.glsl
@@ -14,8 +14,10 @@ uniform sampler2D u_color_specular_texture;
 void main( )
 {
     const vec4 position = texture( u_position_texture, IN.tex_coord );
-    const vec4 normal = texture( u_normal_texture, IN.tex_coord );
+    const vec4 normal = texture( u_normal_texture, IN.tex_coord ) * 2 - 1;
     const vec4 color_specular = texture( u_color_specular_texture, IN.tex_coord );
+    const vec3 color = color_specular.rgb;
+    const float specular = color_specular.a;
 
-    o_color = vec4( 1 );
+    o_color = position;
 }
