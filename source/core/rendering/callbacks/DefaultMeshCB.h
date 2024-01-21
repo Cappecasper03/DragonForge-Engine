c78e60245d60ac900c95328f5d44c4c5e5610037
Cappecasper03 <03repsac@gmail.com>
2024-01-21 22:34:37 +0100

    Added default render callback for forward rendering

--- source/core/rendering/callbacks/DefaultMeshCB.h
+++ source/core/rendering/callbacks/DefaultMeshCB.h
@@ -9,6 +9,42 @@
 
 namespace df::render_callback
 {
+    inline void defaultMeshAmbient( const cShader* _shader, const cMesh* _mesh )
+    {
+#if PROFILING
+        PROFILING_SCOPE( __FUNCTION__ );
+#endif
+
+        const cCamera* camera = cCameraManager::getInstance()->current;
+
+        _shader->use();
+
+        _shader->setUniformMatrix4F( "u_model", _mesh->transform.world );
+        _shader->setUniformMatrix4F( "u_projection_view", camera->projection_view );
+
+        _shader->setUniformSampler( "u_color_texture", 0 );
+        _mesh->textures.at( "color" )->bind();
+
+        glEnable( GL_DEPTH_TEST );
+        glBindVertexArray( _mesh->vertex_array );
+
+        glDrawElements( GL_TRIANGLES, static_cast< GLsizei >( _mesh->indices.size() ), GL_UNSIGNED_INT, nullptr );
+
+        glDisable( GL_DEPTH_TEST );
+    }
+
+    inline void defaultMesh( const cShader* _shader, const cMesh* _mesh )
+    {
+#if PROFILING
+        PROFILING_SCOPE( __FUNCTION__ );
+#endif
+
+        const std::string_view name( _shader->name );
+
+        if( name.find( "ambient" ) != std::string::npos )
+            defaultMeshAmbient( _shader, _mesh );
+    }
+
     inline void defaultMeshDeferred( const cShader* _shader, const cMesh* _mesh )
     {
 #if PROFILING
