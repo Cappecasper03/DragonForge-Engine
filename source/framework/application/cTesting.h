a202c9df2a30ad6385b7bb7cadfae170f48e8154
Cappecasper03 <03repsac@gmail.com>
2024-07-01 21:38:26 +0200

    Kinda got working vulkan quad

--- source/framework/application/cTesting.h
+++ source/framework/application/cTesting.h
@@ -9,6 +9,7 @@
 #include "engine/rendering/iRenderer.h"
 #include "engine/rendering/OpenGL/assets/cQuad_opengl.h"
 #include "engine/rendering/vulkan/cRenderer_vulkan.h"
+#include "engine/rendering/vulkan/descriptor/sDescriptorWriter_vulkan.h"
 #include "engine/rendering/vulkan/misc/Helper_vulkan.h"
 #include "engine/rendering/vulkan/pipeline/cPipeline_vulkan.h"
 #include "engine/rendering/vulkan/pipeline/sPipelineCreateInfo.h"
@@ -22,8 +23,7 @@ struct vertex
 
 struct constants
 {
-	glm::mat4       matrix;
-	VkDeviceAddress address;
+	glm::mat4 matrix;
 };
 
 class cTesting
@@ -44,7 +44,9 @@ public:
 
 	df::vulkan::sAllocatedBuffer vertex_buffer;
 	df::vulkan::sAllocatedBuffer index_buffer;
-	VkDeviceAddress              vertex_buffer_address;
+
+	df::vulkan::sAllocatedBuffer uniform_buffer;
+	VkDescriptorSetLayout        uniform_layout;
 };
 
 inline cTesting::cTesting()
@@ -61,7 +63,7 @@ inline cTesting::cTesting()
 	df::cEventManager::subscribe( df::event::imgui, this, &cTesting::imgui );
 	df::cEventManager::subscribe( df::event::input, this, &cTesting::input );
 
-	df::cRenderer::getRenderInstance()->setCursorInputMode( GLFW_CURSOR_NORMAL );
+	df::cRenderer::getRenderInstance()->setCursorInputMode( GLFW_CURSOR_DISABLED );
 
 	{
 		{
@@ -74,7 +76,7 @@ inline cTesting::cTesting()
 			vertices[ 3 ].position = { -0.5, 0.5, 0 };
 
 			vertices[ 0 ].color = { 0, 0, 0, 1 };
-			vertices[ 1 ].color = { 0.5, 0.5, 0.5, 1 };
+			vertices[ 1 ].color = { 0, 0, 1, 1 };
 			vertices[ 2 ].color = { 1, 0, 0, 1 };
 			vertices[ 3 ].color = { 0, 1, 0, 1 };
 
@@ -93,25 +95,18 @@ inline cTesting::cTesting()
 			const size_t vertex_buffer_size = sizeof( *vertices.data() ) * vertices.size();
 			const size_t index_buffer_size  = sizeof( *indices.data() ) * indices.size();
 
-			vertex_buffer = df::vulkan::helper::util::createBuffer( vertex_buffer_size,
-			                                                        VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_SHADER_DEVICE_ADDRESS_BIT,
-			                                                        VMA_MEMORY_USAGE_GPU_ONLY );
+			vertex_buffer = df::vulkan::helper::util::createBuffer( vertex_buffer_size, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT, VMA_MEMORY_USAGE_GPU_ONLY );
 
 			index_buffer = df::vulkan::helper::util::createBuffer( index_buffer_size, VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT, VMA_MEMORY_USAGE_GPU_ONLY );
 
-			const VkBufferDeviceAddressInfo address_info{
-				.sType  = VK_STRUCTURE_TYPE_BUFFER_DEVICE_ADDRESS_INFO,
-				.buffer = vertex_buffer.buffer,
-			};
+			const df::vulkan::sAllocatedBuffer staging_buffer
+				= df::vulkan::helper::util::createBuffer( vertex_buffer_size + index_buffer_size, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VMA_MEMORY_USAGE_CPU_ONLY );
 
-			vertex_buffer_address = vkGetBufferDeviceAddress( renderer->logical_device, &address_info );
-
-			df::vulkan::sAllocatedBuffer staging_buffer = df::vulkan::helper::util::createBuffer( vertex_buffer_size + index_buffer_size, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VMA_MEMORY_USAGE_CPU_ONLY );
-
-			void* data = staging_buffer.allocation_info.pMappedData;
-
+			void* data;
+			vmaMapMemory( renderer->memory_allocator, staging_buffer.allocation, &data );
 			std::memcpy( data, vertices.data(), vertex_buffer_size );
 			std::memcpy( static_cast< char* >( data ) + vertex_buffer_size, indices.data(), index_buffer_size );
+			vmaUnmapMemory( renderer->memory_allocator, staging_buffer.allocation );
 
 			renderer->immediateSubmit(
 				[ & ]( const VkCommandBuffer _buffer )
@@ -125,7 +120,7 @@ inline cTesting::cTesting()
 					vkCmdCopyBuffer( _buffer, staging_buffer.buffer, vertex_buffer.buffer, 1, &vertex_copy );
 
 					const VkBufferCopy index_copy{
-						.srcOffset = 0,
+						.srcOffset = vertex_buffer_size,
 						.dstOffset = 0,
 						.size      = index_buffer_size,
 					};
@@ -134,17 +129,41 @@ inline cTesting::cTesting()
 				} );
 
 			df::vulkan::helper::util::destroyBuffer( staging_buffer );
+
+			uniform_buffer = df::vulkan::helper::util::createBuffer( sizeof( constants ), VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT, VMA_MEMORY_USAGE_CPU_TO_GPU );
 		}
 
 		{
 			df::vulkan::sPipelineCreateInfo pipeline_create_info{};
 
-			constexpr VkPushConstantRange vertex_buffer_range{
-				.stageFlags = VK_SHADER_STAGE_VERTEX_BIT,
-				.offset     = 0,
-				.size       = sizeof( constants ),
+			constexpr VkVertexInputBindingDescription binding_description{
+				.binding   = 0,
+				.stride    = sizeof( vertex ),
+				.inputRate = VK_VERTEX_INPUT_RATE_VERTEX,
 			};
-			pipeline_create_info.push_constant_ranges.push_back( vertex_buffer_range );
+			pipeline_create_info.vertex_input_binding.push_back( binding_description );
+
+			VkVertexInputAttributeDescription attribute_descriptions{
+				.location = 0,
+				.binding  = 0,
+				.format   = VK_FORMAT_R32G32B32A32_SFLOAT,
+				.offset   = offsetof( vertex, vertex::color ),
+			};
+			pipeline_create_info.vertex_input_attribute.push_back( attribute_descriptions );
+
+			attribute_descriptions = {
+				.location = 1,
+				.binding  = 0,
+				.format   = VK_FORMAT_R32G32B32_SFLOAT,
+				.offset   = offsetof( vertex, vertex::position ),
+			};
+			pipeline_create_info.vertex_input_attribute.push_back( attribute_descriptions );
+
+			df::vulkan::sDescriptorLayoutBuilder_vulkan layout_builder{};
+			layout_builder.addBinding( 0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER );
+
+			uniform_layout = layout_builder.build( VK_SHADER_STAGE_VERTEX_BIT );
+			pipeline_create_info.descriptor_layouts.push_back( uniform_layout );
 
 			pipeline_create_info.setShaders( df::vulkan::helper::util::createShaderModule( "vert" ), df::vulkan::helper::util::createShaderModule( "frag" ) );
 			pipeline_create_info.setInputTopology( VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST );
@@ -178,27 +197,41 @@ inline void cTesting::render()
 	df::vulkan::cRenderer_vulkan* renderer       = reinterpret_cast< df::vulkan::cRenderer_vulkan* >( df::cRenderer::getRenderInstance() );
 	const VkCommandBuffer         command_buffer = renderer->getCurrentFrame().command_buffer;
 
+	const constants constant{
+		.matrix = camera->view_projection,
+	};
+
+	void* data;
+	vmaMapMemory( renderer->memory_allocator, uniform_buffer.allocation, &data );
+	std::memcpy( data, &constant, sizeof( constants ) );
+	vmaUnmapMemory( renderer->memory_allocator, uniform_buffer.allocation );
+
+	std::vector< VkDescriptorSet > descriptor_sets;
+	descriptor_sets.push_back( renderer->getCurrentFrame().descriptors.allocate( uniform_layout ) );
+
+	df::vulkan::sDescriptorWriter_vulkan writer_scene;
+	writer_scene.writeBuffer( 0, uniform_buffer.buffer, sizeof( constants ), 0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER );
+	writer_scene.updateSet( descriptor_sets.back() );
+
 	vkCmdBindPipeline( command_buffer, VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline->pipeline );
+
+	vkCmdBindDescriptorSets( command_buffer, VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline->layout, 0, static_cast< uint32_t >( descriptor_sets.size() ), descriptor_sets.data(), 0, nullptr );
 
 	renderer->setViewportScissor();
 
-	const constants push_constants{
-		.matrix  = glm::mat4( 1 ),
-		.address = vertex_buffer_address,
-	};
+	const VkBuffer         vertex_buffers[] = { vertex_buffer.buffer };
+	constexpr VkDeviceSize offsets[]        = { 0 };
+	vkCmdBindVertexBuffers( command_buffer, 0, 1, vertex_buffers, offsets );
 
-	vkCmdPushConstants( command_buffer, pipeline->layout, VK_SHADER_STAGE_VERTEX_BIT, 0, sizeof( constants ), &push_constants );
 	vkCmdBindIndexBuffer( command_buffer, index_buffer.buffer, 0, VK_INDEX_TYPE_UINT32 );
 
-	vkCmdDrawIndexed( command_buffer, 6, 1, 0, 0, 0 );
+	vkCmdDrawIndexed( command_buffer, static_cast< uint32_t >( indices.size() ), 1, 0, 0, 0 );
 
 	camera->endRender();
 }
 
 inline void cTesting::imgui()
-{
-	ImGui::ShowDemoWindow();
-}
+{}
 
 inline void cTesting::input( const df::input::sInput& /*_input*/ )
 {
