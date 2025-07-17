42ab1502b27e21d4c4a8f59d9599bb0b097ab510
Cappecasper03 <03repsac@gmail.com>
2025-07-17 15:18:47 +0200

    Update .gitignore and remove set runtimes

--- xmake.lua
+++ xmake.lua
@@ -2,16 +2,6 @@ PROJECT_NAME = "DragonForge-Engine"
 
 set_project( PROJECT_NAME )
 
-if is_plat( "windows" ) then
-    if is_mode( "Debug" ) then
-        set_runtimes( "MTd" )
-    else
-        set_runtimes( "MT" )
-    end
-elseif is_plat( "linux" ) then
-    set_runtimes( "stdc++_static" )
-end
-
 -- add_repositories( "overrides xmake-repo" )
 
 includes( "xmake/init.lua" )
